606815e5b28ba29dbf7dde232cb62c8c5a9b806a
Александр Дрожкин <Santay2009@yandex.ru>
2022-03-31 21:45:24 +0300

    Ещё 2 ДТО

---
+++ Data/Dto/ClientDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Data.Dto;
+
+public record ClientDto
+{
+    public ClientDto(Guid id, string clientName, string clientSurname, string clientPatronymic, string phone, string email, IEnumerable<Guid> ordersId) 
+        : this(clientName, clientSurname, clientPatronymic, phone, email, ordersId)
+    {
+        Id = id;
+    }
+
+    public ClientDto(string clientName, string clientSurname, string clientPatronymic, string phone, string email, IEnumerable<Guid> ordersId)
+    {
+        ClientName = clientName;
+        ClientSurname = clientSurname;
+        ClientPatronymic = clientPatronymic;
+        Phone = phone;
+        Email = email;
+        OrdersId = ordersId;
+    }
+
+    public Guid Id { get; }
+    public string ClientName { get; }
+    public string ClientSurname { get; }
+    public string ClientPatronymic { get; }
+    public string Phone { get; }
+    public string Email { get; }
+    public IEnumerable<Guid> OrdersId { get; }
+}
---
+++ Data/Dto/OrderClient.cs
@@ -0,0 +1,28 @@
+﻿namespace Data.Dto;
+
+public record OrderClient
+{
+    public OrderClient(DateTime orderCreateAt, string orderAdress, Guid cakeId, Guid conditer, Guid employee, decimal orderSeller)
+    {
+        OrderCreateAt = orderCreateAt;
+        OrderAdress = orderAdress;
+        CakeId = cakeId;
+        Conditer = conditer;
+        Employee = employee;
+        OrderSeller = orderSeller;
+    }
+
+    public OrderClient(Guid id, DateTime orderCreateAt, string orderAdress, Guid cakeId, Guid conditer, Guid employee, decimal orderSeller) 
+        : this(orderCreateAt, orderAdress, cakeId, conditer, employee, orderSeller)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; }
+    public DateTime OrderCreateAt { get; }
+    public string OrderAdress { get; }
+    public Guid CakeId { get; }
+    public Guid Conditer { get; }
+    public Guid Employee { get; }
+    public Decimal OrderSeller { get; }
+}
