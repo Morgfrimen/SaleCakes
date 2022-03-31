72001d30d99af3b8f635ab030f56ee51dcea8055
Александр Дрожкин <Santay2009@yandex.ru>
2022-03-31 21:29:56 +0300

    Ещё один репозиторий

---
+++ Data/Resositoryes/CakeRepos.cs
@@ -0,0 +1,223 @@
+﻿using Data.Abstract;
+using Data.Dto;
+using System.Data.SqlClient;
+
+namespace Data.Resositoryes;
+
+public class CakeRepos : IRepository<CakeDto>
+{
+    private readonly string? _connectionString;
+
+    public CakeRepos(string? connectionStringString)
+    {
+        _connectionString = connectionStringString;
+    }
+
+    public async Task<CakeDto?> GetByIdAsync(Guid id)
+    {
+        SqlConnection connection = default;
+
+        if (_connectionString is null)
+        {
+            return null;
+        }
+
+        try
+        {
+            connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            var sqlExpression = $"Use SaleCakes; SELECT * FROM cake where id = \'{id}\'";
+            var command = new SqlCommand(sqlExpression, connection);
+            var reader = await command.ExecuteReaderAsync();
+
+            if (reader.HasRows)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var dto = id;
+                    var weight = reader.GetValue(1) is decimal ? (decimal)reader.GetValue(1) : 0;
+                    var tiersId = reader.GetValue(2) is Guid ? (Guid)reader.GetValue(2) : default;
+
+                    var decorDto = new CakeDto(dto, weight, tiersId);
+
+                    return decorDto;
+                }
+            }
+
+            return null;
+        }
+        catch (Exception)
+        {
+            //TODO: Logger?
+            return null;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+            await connection.DisposeAsync();
+        }
+    }
+
+    public async Task<IEnumerable<CakeDto>?> GetAllAsync()
+    {
+        SqlConnection connection = default;
+
+        if (_connectionString is null)
+        {
+            return null;
+        }
+
+        try
+        {
+            connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            var sqlExpression = "Use SaleCakes; SELECT * FROM cake";
+            var command = new SqlCommand(sqlExpression, connection);
+            var reader = await command.ExecuteReaderAsync();
+
+            if (reader.HasRows)
+            {
+                var listEmployees = new List<CakeDto>();
+
+                while (await reader.ReadAsync())
+                {
+                    var dto = reader.GetValue(0) is Guid ? (Guid)reader.GetValue(0) : default;
+                    var weight = reader.GetValue(1) is decimal ? (decimal)reader.GetValue(1) : 0;
+                    var tiersId = reader.GetValue(2) is Guid ? (Guid)reader.GetValue(2) : default;
+
+                    var decorDto = new CakeDto(dto, weight, tiersId);
+                    listEmployees.Add(decorDto);
+                }
+
+                return listEmployees;
+            }
+
+            return null;
+        }
+        catch (Exception)
+        {
+            //TODO: Logger?
+            return null;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+            await connection.DisposeAsync();
+        }
+    }
+
+    public async Task<bool> AddAsync(CakeDto entity)
+    {
+        SqlConnection? connection = default;
+
+        if (_connectionString is null)
+        {
+            return false;
+        }
+
+        try
+        {
+            connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            var sqlExpression = $"Use SaleCakes; INSERT INTO cake (weight,tiers) VALUES ({entity.Weight},\'{entity.TiersId}\')";
+            var command = new SqlCommand(sqlExpression, connection);
+            var reader = await command.ExecuteNonQueryAsync();
+
+            if (reader > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        catch (Exception)
+        {
+            //TODO: Logger?
+            return false;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+            await connection.DisposeAsync();
+        }
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        SqlConnection? connection = default;
+
+        if (_connectionString is null)
+        {
+            return false;
+        }
+
+        try
+        {
+            connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            var sqlExpression = $"Use SaleCakes; DELETE cake WHERE id=\'{id}\'";
+
+            var command = new SqlCommand(sqlExpression, connection);
+            var reader = await command.ExecuteNonQueryAsync();
+
+            if (reader > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        catch (Exception)
+        {
+            //TODO: Logger?
+            return false;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+            await connection.DisposeAsync();
+        }
+    }
+
+    public async Task<bool> UpdateAsync(CakeDto entity)
+    {
+        SqlConnection? connection = default;
+
+        if (_connectionString is null)
+        {
+            return false;
+        }
+
+        try
+        {
+            connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            var sqlExpression = "Use SaleCakes; UPDATE cake " +
+                                $"SET weight={entity.Weight}, tiers=\'{entity.TiersId}\' WHERE id=\'{entity.Id}\'";
+            var command = new SqlCommand(sqlExpression, connection);
+            var reader = await command.ExecuteNonQueryAsync();
+
+            if (reader > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        catch (Exception)
+        {
+            //TODO: Logger?
+            return false;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+            await connection.DisposeAsync();
+        }
+    }
+}
