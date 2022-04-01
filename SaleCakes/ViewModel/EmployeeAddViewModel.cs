5ecf667bbedb9b5c8b092f3125980fb8016c44a6
Александр Дрожкин <Santay2009@yandex.ru>
2022-04-02 00:04:59 +0300

    Добавил роль

--- SaleCakes/ViewModel/EmployeeAddViewModel.cs
+++ SaleCakes/ViewModel/EmployeeAddViewModel.cs
@@ -16,15 +16,29 @@ namespace SaleCakes.ViewModel
 
         public ICommand AddEmployee { get; } = new RelayCommand(async (obj) =>
         {
+            var roleTitle = obj as string;
+            var repostRole = new RoleUserRepos(App.ConnectionString);
+            var roles = await repostRole.GetAllAsync();
+            var selectedRole = roles.FirstOrDefault(item=> item.UserRole == roleTitle);
+
+            if (selectedRole is null)
+            {
+                var newRole = await repostRole.AddAsync(new RoleUserDto(roleTitle));
+                roles = await repostRole.GetAllAsync();
+                selectedRole = roles.FirstOrDefault(item => item.UserRole == roleTitle);
+            }
+
             var reposStuffing = new EmployeeRepos(App.ConnectionString);
-            var model = new EmployeeDto("tName", "tSurname","tPatr","88005553535","tEmail");
+            var model = new EmployeeDto(selectedRole.Id,"tName", "tSurname", "tPatr", "88005553535", "tEmail");
             await reposStuffing.AddAsync(model);
             var allStuffing = await reposStuffing.GetAllAsync();
-            var selectStuffing = allStuffing.FirstOrDefault(item => item.Name == "Теcn");
+            var selectStuffing = allStuffing.FirstOrDefault(item => item.AutorizedUserId == selectedRole.Id); 
             var stuffing = obj as CakeAddViewModel.TierContainer;
             stuffing.Stuffig = selectStuffing.Id;
 
         });
+
+        
 
 
         public override void UpdateAllProperty()
