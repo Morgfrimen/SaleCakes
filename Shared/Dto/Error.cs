619763c6c17703b31d3e1728385466505e00fa4e
Александр Дрожкин <Santay2009@yandex.ru>
2022-04-02 15:36:08 +0300

    Добавил глобальные Dto

---
+++ Shared/Dto/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Dto;
+
+public record Error
+{
+    public Error(string message, int? codeError = null)
+    {
+        Message = message;
+        CodeError = codeError;
+    }
+
+    public string Message { get; }
+    public int? CodeError { get; }
+}
---
+++ Shared/Dto/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Dto;
+
+public record Result<T>
+{
+    public Result(T resultOperation)
+    {
+        ResultOperation = resultOperation;
+    }
+
+    public Result(Error error)
+    {
+        Error = error;
+    }
+
+    public T ResultOperation { get; }
+    public Error Error { get; }
+}
