2c1a7e8dceb5b3ac986d585e88ae8f5dbd4a0a29
Александр Дрожкин <Santay2009@yandex.ru>
2022-04-02 15:56:04 +0300

    Добавил интерфейсы для репозиториев

---
+++ Data/Repositories/Abstract/IBaseCrud.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Dto;
+
+namespace Data.Repositories.Abstract;
+
+public interface IBaseCrud<T>
+{
+    Result<T> Add(T entity);
+    Result<T> Delete(Guid id);
+    Result<T> Update(T entity);
+    Result<T> Get(Guid id);
+}
