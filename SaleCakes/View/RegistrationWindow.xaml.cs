65f66aeb3c66123cb66e2fd76160138bcfbc1735
Александр Дрожкин <Santay2009@yandex.ru>
2022-04-04 22:27:08 +0300

    Почти закончена регистрация

--- SaleCakes/View/RegistrationWindow.xaml.cs
+++ SaleCakes/View/RegistrationWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SaleCakes.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SaleCakes.View
 {
@@ -19,8 +8,9 @@ namespace SaleCakes.View
     /// </summary>
     public partial class RegistrationWindow : Window
     {
-        public RegistrationWindow()
+        public RegistrationWindow(RegistrationViewModel vm)
         {
+            DataContext = vm;
             InitializeComponent();
         }
 
