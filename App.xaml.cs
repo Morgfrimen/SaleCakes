940f723ecaade3e310031db4990a2b55f3d6b8ab
MorgFrimen <Santay2009@yandex.ru>
2022-04-19 22:18:56 +0300

    Добавил в проект MAUI

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SaleCake;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SaleCake;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
